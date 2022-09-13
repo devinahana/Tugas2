36ce6c9de24674656745cd96d14476b3744303b1
devinahana <devina.hana@ui.ac.id>
2022-09-13 14:45:26 +0700

    Make katalog

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+    'list_katalog': data_barang_katalog,
+    'nama': 'Devina Hana',
+    'id': '2106751032',
+    }
+    return render(request, "katalog.html", context)
+
