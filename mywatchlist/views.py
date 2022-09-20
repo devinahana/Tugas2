4e1e94d5d87539ecf3061ac488b1584eb5b1b587
devinahana <devina.hana@ui.ac.id>
2022-09-20 20:20:56 +0700

    Membuat aplikasi mywatchlist

--- mywatchlist/views.py
+++ mywatchlist/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from mywatchlist.models import MyWatchList
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
+def show_mywatchlist(request):
+    data = MyWatchList.objects.all()
+    context = {
+        'movie_list': data,
+        'nama': 'Devina Hana',
+        'id': 2106751032
+    }
+    return render(request, "mywatchlist.html", context)
+
+def show_mywatchlist_xml(request):
+    data = MyWatchList.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_mywatchlist_json(request):
+    data = MyWatchList.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_mywatchlist_html(request):
+    data = MyWatchList.objects.all()
+    return HttpResponse(serializers.serialize("html", data), content_type="application/html")
